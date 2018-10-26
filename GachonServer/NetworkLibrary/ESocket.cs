9e7dc1fa471dd9dc12dd51248d78eeb94db22e98
Seunghwa <banksemi@naver.com>
2018-10-27 07:59:06 +0900

    Start basic function for socket
    Connection between server and client

---
+++ GachonServer/NetworkLibrary/ESocket.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json;
+using System.Runtime.InteropServices;
+namespace NetworkLibrary
+{
+    public class ESocket
+    {
+        TcpClient tcpClient = null;
+        NetworkStream NS = null;
+        StreamReader SR = null;
+        StreamWriter SW = null;
+        public ESocket(TcpClient client)
+        {
+            tcpClient = client;
+            NS = client.GetStream();
+            SR = new StreamReader(NS, Encoding.UTF8); // Get message
+            SW = new StreamWriter(NS, Encoding.UTF8); // Send message
+        }
+        public void Start()
+        {
+
+        }
+        public void Send(string message)
+        {
+
+        }
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ GachonServer/NetworkLibrary/Server.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json;
+using System.Runtime.InteropServices;
+namespace NetworkLibrary
+{
+    public class Server
+    {
+        private TcpListener Listener;
+        Thread Update_Thread = null;
+        public int port { get; private set; }
+        public Server(int port)
+        {
+            this.port = port;
+            Listener = new TcpListener(port);
+            Listener.Start();
+            Update_Thread = new Thread(Update);
+            Update_Thread.Start();
+        }
+        void Update()
+        {
+            while (true)
+            {
+                ESocket client = null;
+                try
+                {
+                    client = new ESocket(Listener.AcceptTcpClient());
+                    client.Start();
+                }
+                catch(Exception e)
+                {
+
+                }
+            }
+        }
+    }
+}
