b5d860734503e5bafadd0b7b847bfbc8605153ca
Seunghwa <banksemi@naver.com>
2018-10-27 15:21:21 +0900

    Create network test project
    Content : 
    Client send "hi"
    Server receive this data, and return same message;

---
+++ NetworkTestCode/MainClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetworkLibrary;
+namespace MainClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client server = new Client("127.0.0.1",10000);
+            server.Connect += Server_Connect;
+            server.Receive += Server_Receive;
+            server.Exit += Server_Exit;
+            server.Start();
+            while (true)
+            {
+                System.Threading.Thread.Sleep(1000);
+                server.Send("Hi");
+            }
+        }
+
+        private static void Server_Receive(ESocket socket, string Message)
+        {
+            Console.WriteLine(socket.GetHashCode() + " : " + Message);
+        }
+
+        private static void Server_Exit(ESocket socket)
+        {
+            Console.WriteLine(socket.GetHashCode() + " exit");
+        }
+
+        private static void Server_Connect(ESocket socket)
+        {
+            Console.WriteLine(socket.GetHashCode() + " connected");
+        }
+    }
+}
---
+++ NetworkTestCode/MainServer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetworkLibrary;
+namespace MainServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server(10000);
+            server.Connect += Server_Connect;
+            server.Receive += Server_Receive;
+            server.Exit += Server_Exit;
+            while(true)
+            {
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+
+        private static void Server_Receive(ESocket socket, string Message)
+        {
+            Console.WriteLine(socket.GetHashCode() + " : " + Message);
+            // return
+            socket.Send(Message);
+        }
+
+        private static void Server_Exit(ESocket socket)
+        {
+            Console.WriteLine(socket.GetHashCode() + " exit");
+        }
+
+        private static void Server_Connect(ESocket socket)
+        {
+            Console.WriteLine(socket.GetHashCode() + " connected");
+        }
+    }
+}
