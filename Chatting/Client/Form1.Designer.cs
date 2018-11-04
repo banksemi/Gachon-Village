004ccbfb8003ae6c75648b3621aca59577e3b8e7
juuyoungjeon <bnb3228@naver.com>
2018-11-04 15:10:21 +0900

    Edit Client server_base structure
    include basic design and simple chatting function

--- Chatting/Client/Form1.Designer.cs
+++ Chatting/Client/Form1.Designer.cs
@@ -29,12 +29,64 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(209, 98);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(442, 109);
+            this.textBox2.TabIndex = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(209, 213);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(383, 25);
+            this.textBox3.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(614, 206);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 31);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Chatting/Client/Form1.cs
+++ Chatting/Client/Form1.cs
@@ -7,14 +7,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using NetworkLibrary;
+using Newtonsoft.Json.Linq;
 namespace Client
 {
     public partial class Form1 : Form
     {
+        NetworkLibrary.Client server;
         public Form1()
         {
+            server = new NetworkLibrary.Client("127.0.0.1",1000);
+            server.Connect += Server_Connect;
+            server.Receive += Server_Receive;
+            server.Exit +=  Server_Exit;
+            server.Start();
             InitializeComponent();
+        }
+
+        private void Server_Connect(ESocket socket)
+        {
+            textBox2.AppendText("연결됨\r\n");
+        }
+
+        private void Server_Receive(ESocket socket, JObject Message)
+        {
+            if ((int)Message["type"]==0)
+            {
+                textBox2.AppendText(Message["message"]+"\r\n");
+            }
+        }
+
+        private void Server_Exit(ESocket socket)
+        {
+            textBox2.AppendText("종료됨\r\n");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            JObject json = new JObject();
+            json["type"] = 0;
+            json["message"] = textBox3.Text;
+            textBox3.Text = "";
+
+            server.Send(json);
         }
     }
 }
