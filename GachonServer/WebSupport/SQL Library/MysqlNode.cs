8c92c4808a30a408822ba2d6aa9b7e71318efb23
Seunghwa <banksemi@naver.com>
2018-11-13 18:57:53 +0900

    Implement #45 - Mysql Library
    Close #45
    MysqlNode do Auto Connect and Close
    
    How to use
    MysqlOption option = new MysqlOption("ip", "database", "id", "password");
    MysqlNode node = new MysqlNode(option, "SELECT * from course where type=?num");
    node["num"] = "'";
    using (node.ExecuteReader()) {
        while(node.Read()) Console.WriteLine(node.GetString("name"));
    }

---
+++ GachonServer/WebSupport/SQL Library/MysqlNode.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using MySql.Data.MySqlClient;
+namespace SQL_Library
+{
+    public class MysqlNode : IDisposable
+    {
+        private string sql;
+        private MysqlOption option;
+        private MySqlConnection conn = null;
+        private Dictionary<String, object> Parameters = new Dictionary<string, object>();
+        public MySqlDataReader Reader { get; private set; }
+        #region
+        public string GetString(string name)
+        {
+            return Reader.GetString(name);
+        }
+        #endregion
+        public MysqlNode(MysqlOption option ,string sql)
+        {
+            this.option = option;
+            ChangeSql(sql);
+        }
+        public void ChangeSql(string sql)
+        {
+            this.sql = sql;
+        }
+
+        public bool Read()
+        {
+            return Reader.Read();
+        }
+        public void AddParameter(string index, object data)
+        {
+            if (ContainParameter(index)) Parameters[index] = data;
+            else
+            {
+                Parameters.Add(index, data);
+            }
+        }
+        public bool ContainParameter(string index)
+        {
+            if (Parameters.ContainsKey(index)) return true;
+            return false;
+        }
+        public object this[string index]    // Indexer declaration  
+        {
+            get
+            {
+                if (ContainParameter(index)) return Parameters[index];
+                else return null;
+            }
+            set
+            {
+                AddParameter(index, value);
+            }
+        }
+        public void AllClose()
+        {
+            if (Reader != null) Reader.Close();
+            if (conn != null) conn.Close();
+            conn = null;
+            Reader = null;
+        }
+        private MySqlCommand Open()
+        {
+            AllClose(); // 모든 객체 닫기
+            conn = new MySqlConnection(option.ToString());
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            foreach (string key in Parameters.Keys)
+            {
+                cmd.Parameters.Add(new MySqlParameter(key, Parameters[key].ToString()));
+            }
+            return cmd;
+        }
+        public MysqlNode ExecuteReader()
+        {
+            MySqlCommand cmd = Open();
+            Reader = cmd.ExecuteReader();
+            return this;
+        }
+        public int ExecuteNonQuery()
+        {
+            MySqlCommand cmd = Open();
+            int result = cmd.ExecuteNonQuery();
+            AllClose();
+            return result;
+        }
+        public void Dispose()
+        {
+            AllClose();
+        }
+    }
+}
