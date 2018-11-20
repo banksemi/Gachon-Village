66a621bed822246b50e63cd1687abe9b07eea13e
Unknown <banksemi@naver.com>
2018-11-20 22:26:30 +0900

    Fix #55 - UI Close Button (working)
    Run the Close command with reference to the Window of the parent object.

---
+++ Unity Client/Gachon Vilage/Assets/Script/UI/Window/CloseButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseButton : MonoBehaviour {
+    private Window parentwindow;
+	// Use this for initialization
+	void Start () {
+        parentwindow = transform.parent.GetComponent<Window>();
+    }
+    void OnClick()
+    {
+        parentwindow.Close();
+    }
+}
