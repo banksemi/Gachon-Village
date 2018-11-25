8028b19fd0ff6f2754484844a8d4ec4847db6fd8
Unknown <banksemi@naver.com>
2018-11-25 12:53:13 +0900

    Fix error that can't insert null in MysqlNode
    In some cases, you may need to put null in the database.
    I modified this part because ToString () for null returns an error.

--- GachonServer/WebSupport/SQL Library/MysqlNode.cs
+++ GachonServer/WebSupport/SQL Library/MysqlNode.cs
@@ -91,7 +91,7 @@ namespace SQL_Library
             MySqlCommand cmd = new MySqlCommand(sql, conn);
             foreach (string key in Parameters.Keys)
             {
-                cmd.Parameters.Add(new MySqlParameter(key, Parameters[key].ToString()));
+                    cmd.Parameters.Add(new MySqlParameter(key, Parameters[key]));
             }
             return cmd;
         }
