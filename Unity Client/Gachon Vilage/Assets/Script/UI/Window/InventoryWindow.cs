a17acbbd2b4072178823dbcf45f9c97b73cb941f
Seunghwa <banksemi@naver.com>
2018-11-30 23:51:36 +0900

    Fix error of overlapping file item
    When new items come in while inventory is closed, 
    Fixed an error that does not automatically sort.

--- Unity Client/Gachon Vilage/Assets/Script/UI/Window/InventoryWindow.cs
+++ Unity Client/Gachon Vilage/Assets/Script/UI/Window/InventoryWindow.cs
@@ -9,6 +9,11 @@ public class InventoryWindow : Window
     public Dictionary<int, FileItem> Items = new Dictionary<int, FileItem>();
     public UIGrid Items_Form;
     public FileItem Prefab_Item;
+    public override void Open()
+    {
+        Items_Form.repositionNow = true;
+        base.Open();
+    }
     public void Add(JObject json)
     {
         FileItem item = Instantiate(Prefab_Item, Items_Form.transform);
