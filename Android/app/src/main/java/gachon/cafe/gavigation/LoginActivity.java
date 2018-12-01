3996ed1a824750bcf84861eeb4bf3e648a05c5db
Seunghwa <banksemi@naver.com>
2018-12-01 18:38:11 +0900

    Change the First Activity
    In addition, I have changed the settings of login layout.

---
+++ Android/app/src/main/java/gachon/cafe/gavigation/LoginActivity.java
@@ -0,0 +1,13 @@
+package gachon.cafe.gavigation;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
+
+
