563c45ea299c945c768a8e8636197c3a598e041a
Seunghwa <banksemi@naver.com>
2018-12-02 22:03:54 +0900

    Implement Saving Login ID
    User can store the ID conveniently in Unity.
    If User enters ID Save, the ID will be saved to disk in real time.
    When User runs the program, the saved ID will be loaded.

--- Unity Client/Gachon Vilage/Assets/Script/Login.cs
+++ Unity Client/Gachon Vilage/Assets/Script/Login.cs
@@ -2,16 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Newtonsoft.Json.Linq;
+using System.IO;
 public class Login : MonoBehaviour
 {
     public UIInput ID;
     public UIInput Password;
-
+    public UIToggle IDToggle; 
     // Use this for initialization
     void Start () {
-		
+        if (File.Exists("idsave.txt"))
+        {
+            string id = File.ReadAllText("idsave.txt");
+            ID.value = id;
+            IDToggle.value = true;
+
+            Password.isSelected = true;
+        }
+        else
+        {
+            ID.isSelected = true;
+        }
 	}
-	
+	public void IDToggleChange()
+    {
+        if (IDToggle.value == true)
+        {
+            File.WriteAllText("idsave.txt", ID.value);
+        }
+        else
+        {
+            if (File.Exists("idsave.txt")) File.Delete("idsave.txt");
+        }
+    }
+    public void ChangeValue()
+    {
+        if (IDToggle.value == true)
+        {
+            File.WriteAllText("idsave.txt", ID.value);
+        }
+    }
 	// Update is called once per frame
 	void Update () {
 		
